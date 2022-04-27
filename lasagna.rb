b782baee88b674baa3915a5fe0b79711caed79a5
samuel verneck <102948001+osamucaaaa@users.noreply.github.com>
2022-04-27 12:35:22 -0300

    Update lasagna.rb

--- lasagna.rb
+++ lasagna.rb
@@ -3,17 +3,14 @@ class Lasagna
   PREPERATION_MINUTES_PER_LAYER = 2
   
   def remaining_minutes_in_oven(actual_minutes_in_oven)
-    raise 'Please implement the Lasagna#remaining_minutes_in_oven method'
-      EXPECTED_MINUTES_IN_OVEN - actual_minutes_in_oven
+    EXPECTED_MINUTES_IN_OVEN - actual_minutes_in_oven
   end
 
   def preparation_time_in_minutes(layers)
-    raise 'Please implement the Lasagna#preparation_time_in_minutes method'
-      layers * PREPERATION_MINUTES_PER_LAYER
+    layers * PREPERATION_MINUTES_PER_LAYER
   end
 
   def total_time_in_minutes(number_of_layers:, actual_minutes_in_oven:)
-    raise 'Please implement the Lasagna#total_time_in_minutes method'
-      preparation_time_in_minutes(number_of_layers) + actual_minutes_in_oven
+    preparation_time_in_minutes(number_of_layers) + actual_minutes_in_oven
   end
 end
